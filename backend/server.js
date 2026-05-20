3b99f4a9b3b5a0dd5a740acb2283bfddac6bffaf
23eg105d62-ui <23eg105d62@anurag.edu.in>
2026-05-20 09:33:45 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ config();
 const app = exp();
 //add cors
 app.use(cors({
-  origin:['http://localhost:5173']
+  origin:['https://user-management-mini-app-cm7l.vercel.app/']
 }))
 // Add body parser middleware
 app.use(exp.json());
